3650be7b850d43c15c22e5c9bd3ab61ba18532cb
Daniel Nephin <dnephin@yelp.com>
2012-01-31 18:51:59 -0800

    Add converter.

--- fpstats/fpstats.py
+++ fpstats/fpstats.py
@@ -63,8 +63,17 @@ rules = [
 ]
 
 """
+import itertools
 import marshal
+import operator
+import re
 from config import INCLUDE, EXCLUDE, START, END, EQ
+
+
+# TODO: filter by caller to a single servlet action
+# TODO: test
+# TODO: look into CPU seconds difference
+
 
 class FilteredStats(object):
 
@@ -75,31 +84,38 @@ class FilteredStats(object):
         self.filtered_stats = {}
         self.load_config(config)
 
-    def build_match_func(self, match_type, rule):
-        """Create a closure that matches the param to the rule."""
+    def matches_rule(self, func_def, rule):
+        if len(rule) == 3:
+            action, match_type, filename = rule
+            funcname = None
+        else:
+            action, match_type, filename, funcname = rule
+
+        target_filename, linenum, target_funcname = func_def
+        funcmatch = not funcname or funcname == target_funcname
+
         if match_type == EQ:
-            return lambda name: name == rule
+            return funcmatch and filename == target_filename
         elif match_type == START:
-            return lambda name: name.startswith(rule)
+            return funcmatch and target_filename.startswith(filename)
         elif match_type == END:
-            return lambda name: name.endswith(rule)
+            return funcmatch and target_filename.endswith(filename)
         else:
             raise ValueError("Unknown match type %s" % match_type)
 
     def load_config(self, config):
         self.rules = []
-        for action, match_type, rule in config:
-            match_func = self.build_match_func(match_type, rule)
-            self.rules.append((action, match_func))
+        for rule in config.rules:
+            # TODO: validate rules
+            self.rules.append(rule)
 
     def include(self, func):
         """Check if this function is included or excluded by our rules.
         Returns true if the function is included.
         """
-        filename, line_no, funcname = func
-        for action, match_func in self.rules:
-            if match_func(filename):
-                return action
+        for rule in self.rules:
+            if self.matches_rule(func, rule):
+                return rule[0]
 
         return True
 
@@ -139,14 +155,85 @@ class FilteredStats(object):
             marshal.dump(self.filtered_stats, f)
 
 
+
+class Converter(object):
+    """Combine different versions of code into a unified version."""
+
+    def __init__(self, pstats, config):
+        self.pstats = pstats
+        self.combined_stats = {}
+        self.load_config(config)
+
+
+    def load_config(self, config):
+        self.converters = []
+        for idx, find, replace in config.converters:
+            self.converters.append((idx, re.compile(find), replace))
+
+
+    def apply_conversions(self, func_def):
+        func_def = list(func_def)
+        for idx, find_pattern, replace in self.converters:
+            func_def[idx] = find_pattern.sub(replace, func_def[idx])
+        return tuple(func_def)
+
+
+    def combine_callers(self, new_callers, old_callers):
+        callers = {}
+        for c_func, c_stats in itertools.chain(
+                new_callers.iteritems(), old_callers.iteritems()):
+            old_stats = callers.get(c_func)
+            if not old_stats:
+                callers[c_func] = c_stats
+                continue
+
+            callers[c_func] = tuple(map(operator.add, old_stats, c_stats))
+        return callers
+
+
+    def combine(self, func_def, stats, new_callers):
+        old_stats = self.combined_stats.get(func_def)
+        if not old_stats:
+            self.combined_stats[func_def] = stats[:4] + (new_callers,)
+            return
+        
+        combined_stats = map(operator.add, old_stats[:4], stats[:4])
+        combined_stats.append(self.combine_callers(new_callers, old_stats[4]))
+        self.combined_stats[func_def] = tuple(combined_stats)
+
+
+    def process_stats(self):
+        for func, stats in self.pstats.stats.iteritems():
+            new_func = self.apply_conversions(func)
+            new_callers = dict(
+                (self.apply_conversions(c_func), c_stats)
+                for c_func, c_stats in stats[4].iteritems()
+            )
+            self.combine(new_func, stats, new_callers)
+        return self.combined_stats
+
+    # TODO: base class
+    def write(self, filename):
+        with open(filename, 'w') as f:
+            marshal.dump(self.combined_stats, f)
+
+
 # TODO: proper arg parsing
 if __name__ == '__main__':
     from pstats import Stats
     import sys
     filename = sys.argv[1] 
     p = Stats(filename)
-    config = __import__(sys.argv[2]).rules
+    config = __import__(sys.argv[2])
     fp = FilteredStats(p, config)
     fp.calculate()
     fp.write('output.profile')
+
+    p = Stats('output.profile')
+    c = Converter(p, config)
+    c.process_stats()
+    c.write('converted.profile')
+
+
+
 
